1adf31a2eb96dc70aa0d82b934b0e9772595bd43
Edu <prog.syrius@gmail.com>
2023-08-18 14:07:05 -0300

    feat: add home page

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import PostList from "./components/PostList";
+
+export default function Home() {
+  return (
+    <main className="max-w-3xl mx-auto my-8 px-12 sm:px-16 lg:px-0">
+      <h1 className="text-3xl font-bold font-special mb-1" id="title">Meu blog pessoal</h1>
+      <p className="text-sm">Olá! Eu me chamo Eduardo e esse é o meu blog, onde falarei sobre hábitos, disciplina e, é claro, <span className='text-specialBlue font-semibold'>código</span>.</p>
+      <hr className="bg-ligthGray mt-5 mb-12" />
+      <PostList />
+      <hr className="bg-darkGray mt-12" />
+    </main>
+  )
+}
