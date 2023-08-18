b6891a7e9ce4d3e3e1d2a0480c60198ad5381046
Edu <prog.syrius@gmail.com>
2023-08-18 14:02:54 -0300

    Feat: add post details component

---
+++ src/components/PostCard/PostDetails.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { AiOutlineClockCircle } from 'react-icons/ai'
+import { MdOutlineCalendarMonth } from 'react-icons/md'
+
+interface PostDetailsProps {
+    date: string
+    readingTime: number
+}
+
+const PostDetails = ({ date, readingTime }: PostDetailsProps) => {
+    return (
+        <div className="flex flex-wrap justify-between items-center text-sm text-darkGray">
+            <div className="flex items-center gap-1">
+                <MdOutlineCalendarMonth size={16} className="text-specialBlue" />
+                <span>{date}</span>
+            </div>
+            <div className="flex items-center gap-1">
+                <AiOutlineClockCircle size={16} className="text-specialBlue" />
+                <span>{readingTime} minutos de leitura</span>
+            </div>
+        </div>
+    )
+}
+
+export default PostDetails
