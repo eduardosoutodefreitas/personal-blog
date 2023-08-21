ddb85c2ae9b94c0cfec70bd02f2fb415ec2a18ef
Edu <prog.syrius@gmail.com>
2023-08-21 14:43:38 -0300

    feat: update home page posts section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import PostList from "./components/PostList";
+import LatestPost from "./components/LatestPosts";
 
 export default function Home() {
   return (
@@ -6,7 +6,7 @@ export default function Home() {
       <h1 className="text-3xl font-bold font-special mb-1" id="title">Meu blog pessoal</h1>
       <p className="text-sm">Olá! Eu me chamo Eduardo e esse é o meu blog, onde falarei sobre hábitos, disciplina e, é claro, <span className='text-specialBlue font-semibold'>código</span>.</p>
       <hr className="bg-ligthGray mt-5 mb-12" />
-      <PostList />
+      <LatestPost />
       <hr className="bg-darkGray mt-12" />
     </main>
   )
