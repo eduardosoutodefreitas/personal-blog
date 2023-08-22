a707c0875b9fbd63356413039fe19d3d6936f8ad
Edu <prog.syrius@gmail.com>
2023-08-22 08:45:14 -0300

    feat: change border radious and add formatedReadingTime

--- src/components/PostCard/PostCard.tsx
+++ src/components/PostCard/PostCard.tsx
@@ -8,11 +8,11 @@ interface PostCardProps {
 }
 const PostCard = ({ post }: PostCardProps) => {
     const formatedDate = formatDate(post.date)
+    const formatedReadingTime = Math.ceil(post.readingTime.minutes)
     return (
         <article className="min-w-[285px] w-full md:w-[45%] shadow-md rounded-md">
-
-            <div className="w-full h-64 md:h-56 lg:h-48 relative overflow-hidden rounded-t-md rounded-l">
-                <Image alt={`${post.title} post image`} className="scale-110 transition-all rounded-t-md rounded-l-md duration-400 hover:scale-100" fill src={post.image} style={{ objectFit: "cover" }} />
+            <div className="w-full h-64 md:h-56 lg:h-48 relative overflow-hidden rounded-t-md">
+                <Image alt={`${post.title} post image`} className="scale-110 transition-all rounded-t-md duration-400 hover:scale-100" fill src={post.image} style={{ objectFit: "cover" }} />
             </div>
             <div className="p-3 pb-5">
                 {
@@ -26,7 +26,7 @@ const PostCard = ({ post }: PostCardProps) => {
                 }
                 <h3 className="font-special font-bold">{post.title}</h3>
                 <p className="text-darkGray mt-1 mb-3 text-sm">{post.description}</p>
-                <PostDetails date={formatedDate} readingTime={Math.ceil(post.readingTime.minutes)} />
+                <PostDetails date={formatedDate} readingTime={formatedReadingTime} />
             </div>
 
         </article>
