b26ca469dbbecce2e59313cf8fc634cdf4295c3c
Edu <prog.syrius@gmail.com>
2023-08-24 10:00:59 -0300

    feat: add basic pagination

--- src/app/components/PostsList.tsx
+++ src/app/components/PostsList.tsx
@@ -1,22 +1,33 @@
 'use client'
+import PaginationControls from '@/components/PaginationControls'
 import PostCard from '@/components/PostCard/PostCard'
-import allPosts from '@/content'
 import { SearchArticlesContext } from '@/context/SearchArticles'
+import { usePagination } from '@/hooks'
+import { useSearchParams } from 'next/navigation'
 import { useContext } from 'react'
-const PostList = () => {
-    const { filteredPosts, searchTerm } = useContext(SearchArticlesContext)
+const PostList = ({ }) => {
+    const { filteredPosts, searchTerm } = useContext(SearchArticlesContext);
+    const { end, start } = usePagination()
+    const posts = searchTerm ? filteredPosts : filteredPosts.slice(start, end);
+
     return (
-        <section className="mt-5 flex items-center justify-between flex-wrap gap-5 md:gap-x-0 md:gap-y-5">
-            {filteredPosts.map((post) => (
-                <PostCard key={post._id} post={post} />
-            ))}
-            {filteredPosts.length === 0 && searchTerm.length !== 0 && (
-                <div className="mb-64">
-                    <p>Nenhum post encontrado.</p>
-                </div>
+        <>
+            <section className="my-8 flex items-center justify-between flex-wrap gap-5 md:gap-x-0 md:gap-y-5 min-h-[50vh]">
+                {posts.map((post) => (
+                    <PostCard key={post._id} post={post} />
+                ))}
+                {(posts.length === 0 && searchTerm !== '') && (
+                    <div className='self-start'>
+                        <p>Nenhum post encontrado.</p>
+                    </div>
+                )}
+            </section>
+            {searchTerm === '' && (
+                <PaginationControls />
             )}
-        </section>
-    )
+        </>
+    );
+
 }
 
 export default PostList
---
+++ src/components/PaginationControls.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import { BsCaretLeftFill, BsCaretRightFill } from 'react-icons/bs'
+import { usePagination } from "@/hooks"
+
+const PaginationControls = () => {
+    const { page, totalPages, router } = usePagination()
+    return (
+        <div className="flex justify-between items-center">
+            <div>
+                {page} de {totalPages}
+            </div>
+            <div className="flex items-center gap-2">
+                <button className="p-2 rounded-md bg-specialBlue disabled:bg-specialBlue/50 disabled:text-white/50 text-white grid place-items-center"
+                    disabled={page === 1}
+                    onClick={() => {
+                        router.push(`/articles/?page=${Number(page) - 1}`)
+                    }}>
+                    <BsCaretLeftFill />
+                </button>
+                <button className="p-2 rounded-md bg-specialBlue disabled:bg-specialBlue/50 disabled:text-white/50 text-white grid place-items-center"
+                    disabled={page === totalPages}
+                    onClick={() => {
+                        router.push(`articles/?page=${Number(page) + 1}`)
+                    }}>
+                    <BsCaretRightFill />
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default PaginationControls
