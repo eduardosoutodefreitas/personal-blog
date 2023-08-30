b0cd3ca3ffad46d22c172699dd1c4a95f67cd43a
Edu <prog.syrius@gmail.com>
2023-08-30 11:06:07 -0300

    feat: remove hr tag from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ export default function Home() {
       <p className="text-sm">Olá! Eu me chamo Eduardo e esse é o meu blog, onde falarei sobre hábitos, disciplina e, é claro, <span className='text-specialBlue font-semibold'>código</span>.</p>
       <hr className="bg-ligthGray mt-5 mb-12" />
       <LatestPost />
-      <hr className="bg-darkGray mt-12" />
     </div>
   )
 }
