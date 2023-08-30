c0ddac1dcaa274ad2a776660d29708acdadf5565
Edu <prog.syrius@gmail.com>
2023-08-30 11:10:00 -0300

    feat: change html lang attribut

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body className={`${merriweather.variable} ${poppins.variable} font-poppins text-primary dark:text-white`}>
         <AppProvider>
           <Header />
