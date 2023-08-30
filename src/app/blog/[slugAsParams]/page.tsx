1368900807497456275e927193654b55b922e23a
Edu <prog.syrius@gmail.com>
2023-08-30 09:42:15 -0300

    feat: add posts navigation

---
+++ src/app/blog/[slugAsParams]/page.tsx
@@ -0,0 +1,49 @@
+import Tag from '@/app/components/Tag'
+import { Mdx } from '@/components/Mdx'
+import PostDetails from '@/components/PostCard/PostDetails'
+import allPosts from '@/content'
+import { formatDate } from '@/services/formatDate'
+import Image from 'next/image'
+import { notFound } from 'next/navigation'
+interface PostPageProps {
+    params: {
+        slugAsParams: string
+    }
+}
+
+async function getPostFromParams(slugAsParams: string) {
+    const post = allPosts.find(post => post.slugAsParams === slugAsParams)
+    if (!post) {
+        notFound()
+    }
+    return post
+}
+const PostPage = async ({ params }: PostPageProps) => {
+    const post = await getPostFromParams(params.slugAsParams)
+    const formatedDate = formatDate(post.date)
+    const formatedReadingTime = Math.ceil(post.readingTime.minutes)
+    return (
+        <div className='max-w-3xl mx-auto my-8 px-12 sm:px-16 lg:px-0'>
+            <div>
+                <div className='w-full h-[255px] sm:h-[375px] relative mb-3'>
+                    <Image src={post.image} className='rounded-md object-cover' fill alt={`post about ${post.title}`} />
+                </div>
+                {
+                    post.tags && (
+                        <div className="flex items-center gap-5 w-full mb-3">
+                            {post.tags.map(tag => (
+                                <Tag key={tag} tag={tag} />
+                            ))}
+                        </div>
+                    )
+                }
+                <PostDetails date={formatedDate} readingTime={formatedReadingTime} />
+            </div>
+            <div className='prose mt-5 dark:prose-invert'>
+                <Mdx code={post.body.code} />
+            </div>
+        </div>
+    )
+}
+
+export default PostPage
--- src/components/PostCard/PostCard.tsx
+++ src/components/PostCard/PostCard.tsx
@@ -3,6 +3,7 @@ import { formatDate } from "@/services/formatDate"
 import { Post } from "../../../.contentlayer/generated/types"
 import PostDetails from "./PostDetails"
 import Tag from "@/app/components/Tag"
+import Link from "next/link"
 interface PostCardProps {
     post: Post
 }
@@ -13,7 +14,9 @@ const PostCard = ({ post }: PostCardProps) => {
     return (
         <article className="min-w-[285px] w-full md:w-[45%] shadow-md rounded-md">
             <div className="w-full h-64 md:h-56 lg:h-48 relative overflow-hidden rounded-t-md">
-                <Image alt={`${post.title} post image`} className="scale-110 transition-all rounded-t-md duration-400 hover:scale-100" fill src={post.image} style={{ objectFit: "cover" }} />
+                <Link href={`blog/${post.slugAsParams}`}>
+                    <Image alt={`${post.title} post image`} className="scale-110 transition-all rounded-t-md duration-400 hover:scale-100" fill src={post.image} style={{ objectFit: "cover" }} />
+                </Link>
             </div>
             <div className="p-3 pb-5">
                 {
@@ -25,7 +28,9 @@ const PostCard = ({ post }: PostCardProps) => {
                         </div>
                     )
                 }
-                <h3 className="font-special font-bold">{post.title}</h3>
+                <Link href={`blog/${post.slugAsParams}`}>
+                    <h3 className="font-special font-bold">{post.title}</h3>
+                </Link>
                 <p className="text-darkGray dark:text-secundaryDark mt-1 mb-3 text-sm">{post.description}</p>
                 <PostDetails date={formatedDate} readingTime={formatedReadingTime} />
             </div>
