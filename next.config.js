1abf14f9bef2f65531c57b3bfc580d928b43b402
Edu <prog.syrius@gmail.com>
2023-09-05 10:42:54 -0300

    chore: add unsplash hostname to next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 const { withContentlayer } = require('next-contentlayer')
 
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true, swcMinify: true }
+const nextConfig = {
+    reactStrictMode: true, swcMinify: true, images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+            },
+        ],
+    },
+}
 
 module.exports = withContentlayer(nextConfig)
