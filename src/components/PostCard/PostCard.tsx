c6288e198183fb6acfc5838b0bb37bb89197b1d2
Edu <prog.syrius@gmail.com>
2023-09-05 10:39:30 -0300

    style: change post card style

--- src/components/PostCard/PostCard.tsx
+++ src/components/PostCard/PostCard.tsx
@@ -12,7 +12,7 @@ const PostCard = ({ post }: PostCardProps) => {
     const formatedReadingTime = Math.ceil(post.readingTime.minutes)
 
     return (
-        <article className="min-w-[285px] w-full md:w-[45%] shadow-md rounded-md">
+        <article className="min-w-[285px] w-full md:w-[45%] shadow-md rounded-md h-[388px]">
             <div className="w-full h-64 md:h-56 lg:h-48 relative overflow-hidden rounded-t-md">
                 <Link href={`blog/${post.slugAsParams}`}>
                     <Image alt={`${post.title} post image`} className="scale-110 transition-all rounded-t-md duration-400 hover:scale-100" fill src={post.image} style={{ objectFit: "cover" }} />
@@ -28,11 +28,11 @@ const PostCard = ({ post }: PostCardProps) => {
                         </div>
                     )
                 }
-                <Link href={`blog/${post.slugAsParams}`}>
-                    <h3 className="font-special font-bold">{post.title}</h3>
-                </Link>
-                <p className="text-darkGray dark:text-secundaryDark mt-1 mb-3 text-sm">{post.description}</p>
                 <PostDetails date={formatedDate} readingTime={formatedReadingTime} />
+                <Link href={`blog/${post.slugAsParams}`}>
+                    <h3 className="font-semibold mt-3">{post.title}</h3>
+                </Link>
+                <p className="text-darkGray dark:text-secundaryDark mt-1 text-xs">{post.description}</p>
             </div>
 
         </article>
